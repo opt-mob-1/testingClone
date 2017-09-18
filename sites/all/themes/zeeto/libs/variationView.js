05732a5d8313629904b69c535afa6537171043c3
opt-mob-1 <opt-mob-1@zeetogroup.com>
2017-09-18 13:23:00 -0700

    updates redirect eop

--- sites/all/themes/zeeto/libs/variationView.js
+++ sites/all/themes/zeeto/libs/variationView.js
@@ -1,17 +1,31 @@
+'use strict';
+
 // Grabbing data attributes set in variation view
 var variationView = document.querySelector('.js-variation-view');
 var variationNextPage = variationView.dataset.nextpage;
 var variationPageNumber = variationView.dataset.pagenumber;
+var funnellength = variationView.dataset.funnellength;
+var redirectURL = variationView.dataset.redirecturl;
 
 var linkQueryParameter = document.querySelectorAll('.js-nextpage-utm');
 for (var i = 0; i < linkQueryParameter.length; i++) {
-  var attrSelect = linkQueryParameter[i].getAttribute("href");
-  // Grabs inputted utm_content value from Drupal Sample Content Type {{ field.utm_content }}
-  var caseSensitiveUf = attrSelect.split('utm_content=')[1];
-  // Builds url and appends dynamic image value selected
-  linkQueryParameter[i].href = variationNextPage + '?utm_content=' + caseSensitiveUf;
+    var attrSelect = linkQueryParameter[i].getAttribute("href");
+    // Grabs inputted utm_content value from Drupal Sample Content Type {{ field.utm_content }}
+    var caseSensitiveUf = attrSelect.split('utm_content=')[1];
+    // Builds url and appends dynamic image value selected
+    linkQueryParameter[i].href = variationNextPage + '?utm_content=' + caseSensitiveUf;
 }
+
+if (variationPageNumber == funnellength) {
+    variationNextPage = redirectURL;
+}
+
+if (variationPageNumber > funnellength) {
+    window.location = redirectURL;
+}
+
 var link = document.querySelectorAll('.js-nextpage');
 for (var i = 0; i < link.length; i++) {
-  link[i].setAttribute("href", variationNextPage);
+    link[i].setAttribute("href", variationNextPage);
 }
+
