0a6fa734dce56ef4d57f4759b50ade53bfc5f4a8
opt-mob-1 <opt-mob-1@zeetogroup.com>
2017-10-23 13:35:14 -0700

    wip Removed submodule directories

--- sites/all/themes/zeeto/libs/vendor/lead_id.js
+++
@@ -1,9 +0,0 @@
-    (function() {
-        var s = document.createElement('script');
-        s.id = 'LeadiDscript_campaign';
-        s.type = 'text/javascript';
-        s.async = true;
-        s.src = '//create.lidstatic.com/campaign/7fe33c9e-594f-0acb-5d34-9929917d1d5a.js?snippet_version=2';
-        var LeadiDscript = document.getElementById('LeadiDscript');
-        LeadiDscript.parentNode.insertBefore(s, LeadiDscript);
-    })();
